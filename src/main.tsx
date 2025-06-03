aa65b1c6cfc3fe0469aa89798c7799495c64a3bd
salah ezzat <121319130+salahezzt120@users.noreply.github.com>
2025-06-04 01:46:50 +0300

    SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <SpeedInsights />
+  </React.StrictMode>,
 );
